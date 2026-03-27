a37210357b9a1b6ff2f6ba1599827d65fd902c74
Sujay Gope <115613352+Sujay9939@users.noreply.github.com>
2026-03-27 19:28:58 +0530

    Create calculater.java

---
+++ src/main/java/calculater.java
@@ -0,0 +1,20 @@
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        double a = 10;
+        double b = 5;
+        char op = '+';
+
+        double result = 0;
+
+        switch(op) {
+            case '+': result = a + b; break;
+            case '-': result = a - b; break;
+            case '*': result = a * b; break;
+            case '/': result = a / b; break;
+        }
+
+        System.out.println("Result: " + result);
+    }
+}
